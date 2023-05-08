d8db0c79fcff2b961ac2f42ffb83c5d27ea8e945
Maaz Khan <kmaaz8826@gmail.com>
2023-05-08 15:16:04 +0530

    final draft with css

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,27 @@ const fetcher = (...args) => fetch(...args).then(response => response.json())
 
 function App() {
   const [searchBook, setSearchBook] = useState('');
-  const [shouldSearch, setShouldSearch] = useState(false)
+  const [shouldSearch, setShouldSearch] = useState(false);
+  const [limit, setLimit] = useState(5);
 
-  const {data, error} = useSWR(shouldSearch ? `https://openlibrary.org/search.json?q=${searchBook}` : null, fetcher)
-  console.log(data)
+  const {data, isLoading} = useSWR(shouldSearch ? `https://openlibrary.org/search.json?q=${searchBook}` : null, fetcher)
+  {data && console.log(data.docs)}
 
   useEffect(()=>{
     setSearchBook((data) => {return data.replace(/ /g,'+')})
     setShouldSearch(false)
+    setLimit(5)
   },[searchBook])
 
   return (
-    <div className="App">
-      <UserContext.Provider value={{ data }}>
-      <h1>Search For A Book</h1>
-      <div>
-        <input type="text" onChange={(event) => {setSearchBook(event.target.value)}} />
-        <button onClick={()=>setShouldSearch(true)}>Search Book</button>
-      </div>
-      {data && <SearchResults />}
+    <div className="App w-full min-h-screen h-fit bg-teal-400">
+      <UserContext.Provider value={{ data, isLoading, limit, setLimit }}>
+      <h1 className='text-4xl font-bold'>Search For A Book</h1>
+      <form className='my-2' onSubmit={(event)=>{setShouldSearch(true); event.preventDefault();}}>
+        <input className='p-1 pl-3 rounded-md text-lg shadow-lg' type="text" onChange={(event) => {setSearchBook(event.target.value)}} />
+        <button className='bg-gray-800 text-white p-1 rounded-md ml-2 px-2 shadow-lg hover:shadow-xl'>Search Book</button>
+      </form>
+      <SearchResults />
       </UserContext.Provider>
     </div>
   );
--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -1,15 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {UserContext} from '../Context';
+import '../App.css'
 
 export default function SearchResults(){
-    const {data} = useContext(UserContext);
+    const {data, isLoading, limit, setLimit} = useContext(UserContext);
     
-    return <div>
-        {data.docs.map((books,key) =>{
-            return <div key={key}>
-                <h2>{books.title}</h2>
-                <h3>By {books.author_name}</h3>
-            </div>
+    return <div className="flex flex-col place-items-center">
+        {isLoading ? <div className='text-3xl font-bold'>Loading...</div> : data && <div className="w-1/3">{data.docs.map((books,key) =>{
+            if(key < limit){
+                return <div key={key} className="shadow-lg hover:shadow-xl w-full bg-white my-4 py-4 rounded-lg px-2 flex flex-col place-items-center">
+                    <img src={`https://covers.openlibrary.org/b/id/${books.cover_i}-M.jpg`} className="w-1/3 mt-2 mb-3" alt={books.title} />
+                    <h2 className="text-xl mb-1 font-semibold">{books.title}</h2>
+                    <h3 className="text-lg">By {books.author_name}</h3>
+                </div>
+            }
         })}
+        <button className="bg-gray-800 text-white py-1 shadow-lg hover:shadow-xl px-2 rounded-md mt-1 mb-3" onClick={()=>{setLimit(limit=>limit+5)}}>Load More</button></div>}
     </div>
 }
